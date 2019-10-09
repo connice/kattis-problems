384cc21fa0ca26a651452fa92bf6cd1eb407baa3
connice <54757709+connice@users.noreply.github.com>
2019-10-08 22:01:44 -0400

    Update Jobexpenses.py

--- Jobexpenses.py
+++ Jobexpenses.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[7]:
-
 
 N = int(input())
 kay= list(map(int, input().split()))
@@ -12,19 +7,8 @@ print(abs(K))
     
 
 
-# In[ ]:
 
 
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
