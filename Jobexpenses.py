226b57bf7704f4f31395f08f2e4ec9dd0145f884
connice <54757709+connice@users.noreply.github.com>
2019-10-08 22:00:06 -0400

    Add files via upload

---
+++ Jobexpenses.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+N = int(input())
+kay= list(map(int, input().split()))
+K= sum(i for i in kay if i< 0)
+print(abs(K))
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
